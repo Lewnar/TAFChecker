5785fb22169144a8c8d603e71739bf5c37311877
Aurora <aurora@Auroras-Mac-mini.local>
2019-10-30 15:19:15 -0700

    Updates

---
+++ precache-manifest.a8deaba98316e388be8402513b07132f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d99291564abbff82079fa39e12a327",
+    "url": "/TAFChecker/index.html"
+  },
+  {
+    "revision": "84aeb405597c61c96fba",
+    "url": "/TAFChecker/static/css/main.395e0c27.chunk.css"
+  },
+  {
+    "revision": "9d972d2f2e1d6fef981b",
+    "url": "/TAFChecker/static/js/2.ff514d2b.chunk.js"
+  },
+  {
+    "revision": "84aeb405597c61c96fba",
+    "url": "/TAFChecker/static/js/main.fe788a86.chunk.js"
+  },
+  {
+    "revision": "a497f7a80d7b5092f5c3",
+    "url": "/TAFChecker/static/js/runtime-main.be4fa7cf.js"
+  }
+]);
