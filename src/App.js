0f5a68ed9fe7e2afa76df0be469dcc496bdf6d03
Aurora <aurora@Auroras-Mac-mini.local>
2019-11-13 14:59:05 -0800

    things have been added to the checker

--- src/App.js
+++ src/App.js
@@ -16,15 +16,15 @@ const App = (props) => {
   const TEMPO = /(TEMPO)/;
   const Time = /[0-3][0-9][0-2][0-9][/][0-3][0-9][0-2][0-9]/;
   const Wind = /([0-3][0-9][0]|(VRB))[0-9][0-9]((KT)|(G)[0-9][0-9](KT))/;
-  const Vis = /((9999)|(9000)|(8000)|(7000)|(6000)|(5000)|(4800)|(4700)|(4500)|(4400)|(4000)|(3700)|(3600)|(3400)|(3200)|(3000)|(2800)|(2600)|(2400)|(2200)|(2000)|(1800)|(1700)|(1600)|(1500)|(1400)|(1300)|(1200)|(1100)|(1000)|(0900)|(0800)|(0700)|(0600)|(0500)|(0400)|(0300)|(0200)|(0100)|(0000))/;
-  const Weather = /((RA)|(-RA)|[+RA]|(SN)|(-RASN)|(-FZRA)|(TS)|(-TS)|[+TS]|(TSRA)|(-TSRA)|[+TSRA])/;
+  const Vis = /((9999)|(9000)|(8000)|(7000)|(6000)|(5000)|(4800)|(4700)|(4500)|(4400)|(4000)|(3700)|(3600)|(3400)|(3200)|(3000)|(2800)|(2600)|(2400)|(2200)|(2000)|(1800)|(1700)|(1600)|(1500)|(1400)|(1300)|(1200)|(1100)|(1000)|(0900)|(0800)|(0700)|(0600)|(0500)|(0400)|(0300)|(0200)|(0100)|(0000)|([-]0200))/;
+  const Weather = /((RA)|([-]RA)|([+]RA)|(SN)|([-]SN)|([+]SN)|(TS)|([-]TS)|([+]TS)|(TSRA)|([-]TSRA)|([+]TSRA))/;
   const Sky = /((SKC)|(((FEW)|(SCT)|(BKN)|(OVC))([0-9]{3}|[0-9]{3}(CB)))|((VV)[0-9]{3}))/;
   const Alt = /(QNH)[0-9]{4}(INS)/;
+  const Ice = /(6)[0-9]{5}/;
+  const Turb = /((5)[0-9]{5})/;
   const TX = /((((TX)[0-9]{2})|((TXM)[0-9]{2}))[/][0-3][0-9][0-2][0-9](Z))/;
   const TN = /(((((TN)[0-9]{2})|((TNM)[0-9]{2}))[/][0-3][0-9][0-2][0-9](Z)))/;
   const Error = '';
-  const oLineInit = [/(BECMG)/, Time, Wind, Vis]
-  const oLineTemp = [/(TEMPO)/, Time]
 
 
   //onclick of check TAF causes this function to activate
@@ -45,6 +45,10 @@ const App = (props) => {
       splittingBlock.push(cleaner(blockSplit))
     }
     return splittingBlock
+  }
+
+  const checkHours = () => {
+
   }
 
   //function for the first line of the TAF regex
@@ -71,7 +75,11 @@ const App = (props) => {
         lineContentWx.push(Sky)
       } else if(block.match(Alt)){
         lineContentWx.push(Alt)
-      } else if(block.match(TX)) {
+      } else if(block.match(Ice)){
+        lineContentWx.push(Ice)
+      } else if(block.match(Turb)){
+        lineContentWx.push(Turb)
+      } else if(block.match(TX)){
         lineContentWx.push(TX)
       } else if(block.match(TN)){
         lineContentWx.push(TN)
@@ -85,28 +93,80 @@ const App = (props) => {
 
   //building other Lines of the TAF regex
   const buildRegexOtherLine = (line) => {
-    const Answer = []
-      const otherLineRegex = line.slice(4)
-      Answer.push(otherLineRegex)
-      const lineContent = []
-      for (let Block of otherLineRegex){
-        if(Block.match(Weather)){
-          lineContent.push(Weather)
-        } else if(Block.match(Sky)){
-          lineContent.push(Sky)
-        } else if(Block.match(Alt)){
-          lineContent.push(Alt)
-        } else if(Block.match(TX)) {
-          lineContent.push(TX)
-        } else if (Block.match(TN)){
-          lineContent.push(TN)
-        } else {
-          lineContent.push(Error)
+    const fBlock = line.slice(0,1)
+    let lineContent = []
+    const oLineInit = []
+    for(let other of fBlock){
+      let otherLineRegex = line.slice(4)
+      if(other.match(BECMG)){
+        oLineInit.push(BECMG, Time, Wind, Vis)
+        for (let Block of otherLineRegex){
+          if(Block.match(Weather)){
+            lineContent.push(Weather)
+          } else if(Block.match(Sky)){
+            lineContent.push(Sky)
+          } else if(Block.match(Alt)){
+            lineContent.push(Alt)
+          } else if(Block.match(Ice)){
+            lineContent.push(Ice)
+          } else if(Block.match(Turb)){
+            lineContent.push(Turb)
+          } else if(Block.match(TX)) {
+            lineContent.push(TX)
+          } else if (Block.match(TN)){
+            lineContent.push(TN)
+          } else {
+            lineContent.push(Error)
+          }
+        }
+      } else {
+        oLineInit.push(TEMPO, Time)
+        otherLineRegex = line.slice(2)
+        for (let Block of otherLineRegex){
+          if(Block.match(Wind)){
+            lineContent.push(Wind)
+          } else if(Block.match(Vis)){
+            lineContent.push(Vis)
+          } else if(Block.match(Weather)){
+            lineContent.push(Weather)
+          } else if(Block.match(Sky)){
+            lineContent.push(Sky)
+          } else if(Block.match(Alt)){
+            lineContent.push(Alt)
+          } else if(Block.match(Ice)){
+            lineContent.push(Ice)
+          } else if(Block.match(Turb)){
+            lineContent.push(Turb)
+          } else if(Block.match(TX)) {
+            lineContent.push(TX)
+          } else if (Block.match(TN)){
+            lineContent.push(TN)
+          } else {
+            lineContent.push(Error)
+          }
         }
       }
-    return lineContent
+    }
+    let Data = oLineInit.concat(lineContent)
+    return Data
   }
 
+  const checkForSpecialCase = (block, lineNumber) => {
+    //1218/1324
+    if(block.match(Time) && block.length === 9 && lineNumber === 0){
+      const hour1 = parseInt(block.slice(2,4))
+      const hour2 = parseInt(block.slice(7))
+      if((24 - hour1)+hour2 === 30){
+         return true
+      } else {
+        return false
+      }
+    // } else if(block.match(Sky)){
+    //   const layOut = block
+    //   console.log('========================>', layOut);
+    }
+    return true
+  }
 
   //function that goes through the array and checks if anything is wrong or not there
   const checkingTAF = (split) => {
@@ -116,13 +176,14 @@ const App = (props) => {
       let index = parseInt(line)
       let tafRow = split[index]
       let checkerRow = []
-      if(index === 0){
+      if(index === 0){ //First TAF Line
         checkerRow = regexFirst
-      } else { //General Other line 
-        checkerRow = oLineInit.concat(buildRegexOtherLine(tafRow))
+      } else { //General Other TAF line
+        checkerRow = buildRegexOtherLine(tafRow)
       }
-      let fLineOutput = tafRow.map((item, index) => {
-        if(checkerRow[index] && checkerRow[index].test(item)){
+      let fLineOutput = tafRow.map((item, mapIndex) => {
+        const specialCase = checkForSpecialCase(item, index)
+        if(checkerRow[mapIndex] && checkerRow[mapIndex].test(item) && specialCase){
           return true
         } else {
           return false
